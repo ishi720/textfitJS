a67bb48003946ba8e4c014349eb6ad744f850b2b
ishi720 <ishizuka720@gmail.com>
2023-12-19 22:29:55 +0900

    リファクタリング

--- textfitJS.js
+++ textfitJS.js
@@ -1,24 +1,29 @@
+"use strict";
 class textFit {
+    #orgText;
+    #orgSize;
+
     constructor(selector) {
         this.selector = selector;
-        this.orgText = document.querySelector(this.selector).innerHTML;
-        this.orgSize = window.getComputedStyle(document.querySelector(this.selector), null).getPropertyValue('font-size');
+        this.targetEle = document.querySelector(this.selector);
+        this.#orgText = this.targetEle.innerHTML;
+        this.#orgSize = window.getComputedStyle(this.targetEle, null).getPropertyValue('font-size');
     }
 
-    isProtrudeWidth(ele) {
-        var orgScrollLeft = ele.scrollLeft;
+    #isProtrudeWidth(ele) {
+        const orgScrollLeft = ele.scrollLeft;
         ele.scrollLeft = 0;
         ele.scrollLeft = 1;
-        var result = (ele.scrollLeft > 0);
+        const result = (ele.scrollLeft > 0);
         ele.scrollLeft = orgScrollLeft;
         return result;    
     }
 
-    isProtrudeHeight(ele) {
-        var orgScrollTop = ele.scrollTop;
+    #isProtrudeHeight(ele) {
+        const orgScrollTop = ele.scrollTop;
         ele.scrollTop = 0;
         ele.scrollTop = 1;
-        var result = (ele.scrollTop > 0);
+        const result = (ele.scrollTop > 0);
         ele.scrollTop = orgScrollTop;
         return result;
     }
@@ -30,30 +35,31 @@ class textFit {
     */
     textShrinkCut(leader = "…", repeat = 10) {
         //一時的にスクロールバーをつける
-        document.querySelector(this.selector).style.overflowY = "hidden";
+        this.targetEle.style.overflowY = "hidden";
 
-        for (var i = 0; this.isProtrudeHeight(document.querySelector(this.selector)) === true || i >= repeat ; i++) {
+        for (let i = 0; this.#isProtrudeHeight(this.targetEle) === true || i >= repeat ; i++) {
 
-            if( i >= repeat - 1){
+            if (i >= repeat - 1) {
                 //文字を省略する
                 this.textCut(leader);
-                break;// 繰り返しの上限達した場合はループを抜ける
+                break;
             }
 
             //下限判定(メモ：Chromeの最小値が10px)
-            if ( parseInt(document.querySelector(this.selector).style.fontSize) <= 10 ){
+            if (parseInt(this.targetEle.style.fontSize) <= 10) {
                 //文字を省略する
                 this.textCut();
-                break;// 繰り返しの上限達した場合はループを抜ける
+                break;
+
             } else {
                 //サイズを調整
-                var el = document.querySelector(this.selector);
-                var fontSize = window.getComputedStyle(el, null).getPropertyValue('font-size');
-                document.querySelector(this.selector).style.fontSize = (parseFloat(fontSize) - 1) + 'px';
+                const el = this.targetEle;
+                const fontSize = window.getComputedStyle(el, null).getPropertyValue('font-size');
+                this.targetEle.style.fontSize = (parseFloat(fontSize) - 1) + 'px';
             }
         }
         //スクロールバーを消す
-        document.querySelector(this.selector).style.overflowY = "visible";
+        this.targetEle.style.overflowY = "visible";
     }
 
     /**
@@ -61,14 +67,11 @@ class textFit {
     * @param leader 省略文字
     */
     textCut(leader = "…"){
-        document.querySelectorAll(this.selector).forEach((element, i) => {
-            var str = element.innerHTML;
+        document.querySelectorAll(this.selector).forEach((element) => {
+            let str = element.innerHTML;
             //一時的にスクロールバーをつける
             element.style.overflowY = 'hidden';
-            while (this.isProtrudeHeight(element) === true) {
-                if (str.length <= 0) {
-                    break;
-                }
+            while (this.#isProtrudeHeight(element) && str.length > 0) {
                 //末尾の文字を削る
                 str = str.slice(0, -1); 
                 //文字をセットする
@@ -80,11 +83,20 @@ class textFit {
     }
 
     /**
-    * テキストをもとに戻す
+    * 要素のテキストとフォントサイズを元に戻す
     */
     restoreOriginalText() {
-        // 要素のテキストとフォントサイズを元に戻す
-        document.querySelector(this.selector).innerHTML = this.orgText;
-        document.querySelector(this.selector).style.fontSize = this.orgSize;
+        this.targetEle.innerHTML = this.#orgText;
+        this.targetEle.style.fontSize = this.#orgSize;
+    }
+
+    // 元のテキストのgetter
+    get originalText() {
+        return this.#orgText;
+    }
+
+    // 元のフォントサイズのgetter
+    get originalFontSize() {
+        return this.#orgSize;
     }
 }
